9a6807fa4c02b5c3ca81256ecc5195efbf712030
T-Sugiyama-dev <takumasa.aran@gmail.com>
2025-05-01 23:11:10 +0900

    create footer

---
+++ src/components/features/footer/Footer.tsx
@@ -0,0 +1,37 @@
+/** @jsxImportSource @emotion/react */
+import { FC, memo } from "react";
+import { css } from "@emotion/react";
+
+interface FooterProp {
+  className: string;
+}
+
+const Footer: FC<FooterProp> = memo(({ className }) => {
+  return (
+    <footer css={[footerBase, className === 'absolute' ? footerAbsolute : footerRelative]}>
+      <div css={copyright}>
+        <small>&copy; 2023 Takumasa Sugiyama</small>
+      </div>
+    </footer>
+  );
+});
+
+export default Footer;
+
+const footerBase = css``;
+
+const footerAbsolute = css`
+  position: absolute;
+  bottom: 0;
+`;
+
+const footerRelative = css`
+  position: relative;
+`;
+
+const copyright = css`
+  width: 100vw;
+  text-align: center;
+  color: #ffffff;
+`;
+
