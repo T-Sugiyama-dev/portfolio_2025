c8b0baeca41ee95866a0f02e8eaf66b911ddfa74
T-Sugiyama-dev <takumasa.aran@gmail.com>
2025-05-01 23:16:50 +0900

    create error page

---
+++ src/components/pages/error/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import PageNotFound from "../../features/error/PageNotFound";
+
+const Error404: FC = () => {
+  return <PageNotFound />;
+};
+
+export default Error404;
