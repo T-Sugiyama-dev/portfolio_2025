5efbe8be4311c627ccc7128db81c8d5f86ce2a2a
T-Sugiyama-dev <takumasa.aran@gmail.com>
2025-05-01 23:16:41 +0900

    create work page

---
+++ src/components/pages/work/index.tsx
@@ -0,0 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import { FC } from "react";
+import { css } from "@emotion/react";
+import Transition from "../../features/transition/Transition";
+import Footer from "../../features/footer/Footer";
+import Works from "../../features/work/Works";
+
+const Work: FC = () => {
+  return (
+    <div css={container}>
+      <Transition className="transition_work" text="What I've done" />
+      <Works />
+      <Footer className="absolute" />
+    </div>
+  );
+};
+
+export default Work;
+
+const container = css`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background-color: #c2b7ff;
+`;
