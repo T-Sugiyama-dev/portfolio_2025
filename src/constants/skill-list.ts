2cee6ddac70feddd01499b9b201ca2bdd8822c0b
T-Sugiyama-dev <takumasa.aran@gmail.com>
2025-05-01 23:10:51 +0900

    add constants

---
+++ src/constants/skill-list.ts
@@ -0,0 +1,36 @@
+type SkillContent = {
+  name: string;
+  percent: number;
+};
+
+export const SKILL_LIST: SkillContent[] = [
+  { name: "HTML", percent: 85 },
+  { name: "CSS", percent: 82 },
+  { name: "JavaScript", percent: 85 },
+  { name: "TypeScript", percent: 80 },
+  { name: "React.js", percent: 82 },
+  { name: "Next.js", percent: 76 },
+  { name: "Vue.js", percent: 70 },
+  { name: "PHP", percent: 77 },
+  { name: "Laravel", percent: 70 },
+  { name: "Java", percent: 72 },
+  { name: "C#", percent: 70 },
+  { name: "Spring", percent: 70 },
+  { name: "Python", percent: 65 },
+  { name: "Django", percent: 63 },
+  { name: "FastAPI", percent: 60 },
+  { name: "Go", percent: 75 },
+  { name: "Bash Script", percent: 70 },
+  { name: "VBA", percent: 60 },
+  { name: "Flutter", percent: 52 },
+  { name: "AWS", percent: 35 },
+  { name: "MySQL", percent: 65 },
+  { name: "PostgreSQL", percent: 55 },
+  { name: "Oracle DB", percent: 42 },
+  { name: "SQL Server", percent: 50 },
+  { name: "Figma", percent: 90 },
+  { name: "XD", percent: 75 },
+  { name: "Photoshop", percent: 78 },
+  { name: "Lightroom", percent: 90 },
+  { name: "Premiere Pro", percent: 78 },
+];
